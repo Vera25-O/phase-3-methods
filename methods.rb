15991cb99f68088b8e378631b084a007bcca3996
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-09-02 22:10:18 +0300

    methods in method.rb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+ end
+
+ greet_programmer
+
+ def greet(name ="Naureen" )
+     puts "Hello, #{name}!"
+ end
+
+ greet
+
+ def greet_with_default(name = "programmer")
+     puts "Hello, #{name}!"
+
+ end
+  greet_with_default
+
+  greet_with_default "Sunny"
+
+  def add(num1,num2)
+      num1 + num2
+
+  end
+
+  def halve(num)
+     return nil unless num.class == Integer
+
+     num / 2
+   end
+
